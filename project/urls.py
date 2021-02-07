21ab7da9f89fdb78f781bd172413ae2d023a4578
Osama Muhammad <osamamohammed3623@gmail.com>
2021-02-07 15:45:42 +0200

    updating static files

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('cpg/', include('cpg.urls')),
     path('admin/', admin.site.urls),
-    path('cpg/', include('cpg.urls'))
 ]
